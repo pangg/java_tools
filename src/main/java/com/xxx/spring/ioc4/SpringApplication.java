e06fe1bdfc66ac8de200a3025a153500bfba54d8
pangg <372725807>
2022-03-15 17:40:31 +0800

    spring IoC 查看容器内的对象

--- src/main/java/com/xxx/spring/ioc4/SpringApplication.java
+++ src/main/java/com/xxx/spring/ioc4/SpringApplication.java
@@ -2,6 +2,7 @@ package com.xxx.spring.ioc4;
 
 import com.alibaba.druid.support.json.JSONUtils;
 import com.xxx.spring.ioc4.entity.Company;
+import com.xxx.spring.ioc4.entity.Computer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,6 +13,22 @@ public class SpringApplication {
         System.out.println(company);
         String website = company.getInfo().getProperty("website");
         System.out.println(website);
+
+        System.out.println("=========================");
+
+        // 获取容器内所有beanId数组
+        String[] beanNames = context.getBeanDefinitionNames();
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+            System.out.println("类型：" + context.getBean(beanName).getClass().getName());
+            System.out.println("内容：" + context.getBean(beanName));
+        }
+
+        // 获取 无id和name标识的Bean对象
+        Computer computer = context.getBean("com.xxx.spring.ioc4.entity.Computer", Computer.class);
+        System.out.println(computer.getBrand());
+        Computer computer1 = context.getBean("com.xxx.spring.ioc4.entity.Computer#1", Computer.class);
+        System.out.println(computer1.getBrand());
     }
 
 }
