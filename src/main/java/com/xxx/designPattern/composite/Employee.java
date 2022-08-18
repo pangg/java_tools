3ae6377f3d8d1429884210f63ea68ad827ad1cef
pangg <372725807>
2022-08-18 20:00:22 +0800

    java 组合模式

---
+++ src/main/java/com/xxx/designPattern/composite/Employee.java
@@ -0,0 +1,42 @@
+package com.xxx.designPattern.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建具有Employee对象列表的Employee类。
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        this.subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
