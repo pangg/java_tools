a55d2d678be4911dff02c7dd8f31f53642e75b1e
pangg <372725807>
2022-08-18 16:01:14 +0800

    java 适配器模式

---
+++ src/main/java/com/xxx/designPattern/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,9 @@
+package com.xxx.designPattern.adapter;
+
+/**
+ * 高级媒体播放器接口
+ */
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
