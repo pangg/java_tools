650dc11e12394d2bcb6264f9a13d233a980ead5e
pangg <372725807>
2022-08-26 17:22:29 +0800

    java 策略模式

---
+++ src/main/java/com/xxx/designPattern/strategy/StrategyPatternDemo.java
@@ -0,0 +1,19 @@
+package com.xxx.designPattern.strategy;
+
+/**
+ * 策略模式
+ *
+ * @See https://www.yiibai.com/design_pattern/strategy_pattern.html
+ */
+public class StrategyPatternDemo {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationSubstract());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+}
